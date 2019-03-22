7fad4acd413aab71676000268a5b610b5fecd654
kirbycampbell <jkirbycampbell@gmail.com>
2019-03-22 15:42:37 -0700

    Added Layout, and Database Mutations and Subscriptions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,104 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Amplify from "aws-amplify";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
 import awsmobile from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
+import * as queries from "./graphql/queries";
+import * as mutations from "./graphql/mutations";
+import * as subscriptions from "./graphql/subscriptions";
 
 Amplify.configure(awsmobile);
 
 class App extends Component {
+  state = {
+    message: "",
+    conversation: []
+  };
+
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  };
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  mutate = async () => {
+    const postDeets = {
+      title: "Msg1",
+      body: this.state.message,
+      createdAt: ""
+    };
+    const newPost = await API.graphql(
+      graphqlOperation(mutations.createPost, { input: postDeets })
+    );
+    console.log(newPost);
+  };
+  subscription = API.graphql(
+    graphqlOperation(subscriptions.onCreatePost)
+  ).subscribe({
+    next: newPost =>
+      this.setState(prevState => ({
+        conversation: [
+          ...prevState.conversation,
+          newPost.value.data.onCreatePost
+        ]
+      }))
+  });
+
+  query = async () => {
+    let allPosts = await API.graphql(
+      graphqlOperation(queries.listPosts, { limit: 100 })
+    );
+    let thread = allPosts.data.listPosts.items;
+    let sortedThread = thread.sort((a, b) => {
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+    });
+    this.setState({
+      conversation: sortedThread
+    });
+    console.log(allPosts);
+  };
+
+  typing = event => {
+    this.setState({
+      message: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Chat-Box">
+          {this.state.conversation.map(convo => {
+            return (
+              <p className="text-boxes" key={convo.id}>
+                {convo.body}
+              </p>
+            );
+          })}
+          <div
+            style={{ float: "left", clear: "both" }}
+            ref={el => {
+              this.messagesEnd = el;
+            }}
+          />
+        </div>
+        <div className="Friend-Box">Friend List</div>
+        <div className="Type-Box">
+          <textarea
+            className="txtarea"
+            placeholder="type here"
+            value={this.state.message}
+            onChange={this.typing}
+          />
+        </div>
+        <button onClick={this.mutate}>Mutate</button>
+        <button onClick={this.query}>Query</button>
       </div>
     );
   }
