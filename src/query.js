c197e51148f131de720aeafa0d8e3280263b570f
kirbycampbell <jkirbycampbell@gmail.com>
2019-03-24 18:56:34 -0700

    Switching Query into a Custom Hook

--- src/query.js
+++ src/query.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import * as queries from "./graphql/queries";
 import { API, graphqlOperation } from "aws-amplify";
 
-export default function query() {
+export const query = queryVar => {
   const [users, setUsers] = useState("");
   const [convo, setConvo] = useState([]);
 
@@ -19,6 +19,20 @@ export default function query() {
     graphqlOperation(queries.listUsers, { limit: 10 })
   );
   setUsers(allUsers.data.listUsers.items);
-}
 
-export default query;
+  return [users, convo];
+};
+
+//     let allPosts = API.graphql(
+//     graphqlOperation(queries.listPosts, { limit: 100 })
+//   );
+//   let thread = allPosts.data.listPosts.items;
+//   let sortedThread = thread.sort((a, b) => {
+//     return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+//   });
+
+//   setConvo(sortedThread);
+//   let allUsers = API.graphql(
+//     graphqlOperation(queries.listUsers, { limit: 10 })
+//   );
+//   setUsers(allUsers.data.listUsers.items);
